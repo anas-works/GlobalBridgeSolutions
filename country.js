f9c424191599ab7b64f9242919e101c0e1aec5f2
anas-works <manasejaz.works@gmail.com>
2025-01-06 21:19:36 +0500

    done

--- country.js
+++ country.js
@@ -1,22 +1,32 @@
 const videoBoxes = document.querySelectorAll('.boxes');
 const videos = document.querySelectorAll('.video-s3');
 
-// Loop through each video box and attach event listeners
-videoBoxes.forEach((videoBox, index) => {
-  const video = videos[index]; // Get the corresponding video
-  
+if(window.innerWidth > 500){ 
+  // Loop through each video box and attach event listeners
+  videoBoxes.forEach((videoBox, index) => {
+    const video = videos[index]; // Get the corresponding video
+    
   // Play video on hover
   videoBox.addEventListener('mouseenter', function() {
     video.play();
     video.muted = true; // Muted to avoid autoplay issues
     video.loop = true;  // Loop to play continuously
   });
-
+  
   // Pause video when hover ends
   videoBox.addEventListener('mouseleave', function() {
     video.pause();
   });
 });
+
+}else if (window.innerWidth < 500) {
+  videoBoxes.forEach((videoBox, index) => {
+    const video = videos[index]; // Get the corresponding video
+    video.play();
+    video.muted = true; // Muted to avoid autoplay issues
+    video.loop = true;  // Loop to play continuously
+});
+}
 
 const pkBtn = document.getElementById('PkBox');
 const ukBtn = document.getElementById('ukBox');
